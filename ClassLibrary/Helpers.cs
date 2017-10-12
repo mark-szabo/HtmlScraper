c28fcaa0e4c4516fff1796627b67dae94224775a
Mark Szabo <szmaorka@gmail.com>
2017-10-12 23:34:46 +0200

    Creating Class Library

---
+++ ClassLibrary/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Helpers
+    {
+        public static string UriToString(Uri uri)
+        {
+            return (uri != null) ? uri.ToString() : "";
+        }
+    }
+}
